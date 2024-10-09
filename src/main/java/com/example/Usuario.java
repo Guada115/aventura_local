be4f09f12e39f6156a0ad7d49dd15e23be9eec69
Juanes <juan_marin23232@elpoli.edu.co>
2024-10-09 18:40:06 -0500

    Base de Datos

---
+++ src/main/java/com/example/Usuario.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "USUARIOS")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NOMBRE", nullable = true)
+    private String nombre;
+
+    @Column(name = "APELLIDO", nullable = true)
+    private String apellido;
+
+    @Column(name = "CORREO", nullable = true, unique = true)
+    private String correo;
+
+    @Column(name = "FECHA_NACIMIENTO",nullable = true)
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+
+    @Column(name = "USUARIO", nullable = true, unique = true)
+    private String usuario;
+
+    @Column(name = "CONTRASENA", nullable = true)
+    private String contrasena;
+}
